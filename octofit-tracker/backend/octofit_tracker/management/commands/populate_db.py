e0d5c1b1ebab1948704fe5a8d8aed7efc2e84b82
Chandra Gunda <58647309+gv706@users.noreply.github.com>
2026-01-21 12:34:49 +0000

    Add REST API endpoints, serializers, admin, and tests for all OctoFit models

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Clear existing data
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create Teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create Users
+        tony = User.objects.create(name='Tony Stark', email='tony@marvel.com', team=marvel)
+        steve = User.objects.create(name='Steve Rogers', email='steve@marvel.com', team=marvel)
+        bruce = User.objects.create(name='Bruce Wayne', email='bruce@dc.com', team=dc)
+        clark = User.objects.create(name='Clark Kent', email='clark@dc.com', team=dc)
+
+        # Create Activities
+        Activity.objects.create(user=tony, type='Run', duration=30, date=timezone.now())
+        Activity.objects.create(user=steve, type='Swim', duration=45, date=timezone.now())
+        Activity.objects.create(user=bruce, type='Bike', duration=60, date=timezone.now())
+        Activity.objects.create(user=clark, type='Yoga', duration=20, date=timezone.now())
+
+        # Create Workouts
+        Workout.objects.create(name='Super Strength', description='Heavy lifting and resistance', suggested_for='Marvel')
+        Workout.objects.create(name='Flight Training', description='Aerobic and agility', suggested_for='DC')
+
+        # Create Leaderboard
+        Leaderboard.objects.create(team=marvel, points=100)
+        Leaderboard.objects.create(team=dc, points=90)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
